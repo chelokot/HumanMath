60119ce28cbf2340649a9750f26ab79d9484c43a
chelokot <andrey.vlasenko.work@gmail.com>
2025-06-16 01:19:27 +0300

    proof natural addition commutativity

--- HumanMath/Natural/Add.lean
+++ HumanMath/Natural/Add.lean
@@ -4,7 +4,6 @@
 -/
 
 import HumanMath.Natural.Basic
-
 namespace HumanMath.Natural
 
 /--
@@ -13,18 +12,53 @@ namespace HumanMath.Natural
   - n + 0 = n
   - n + (m + 1) = (n + m) + 1
 -/
-def add : Natural → Natural → Natural
-  | n, Natural.zero => n
-  | n, Natural.next m => Natural.next (add n m)
+def add (n m : Natural): Natural :=
+  match m with
+  | Natural.zero => n
+  | Natural.next m' => Natural.next (add n m')
 
-/-- Add one to a number -/
-def addOne (n : Natural) : Natural := Natural.next n
-
-/-- Add zero to a number (identity function) -/
-def addZero (n : Natural) : Natural := n
-
-/-- Make + operator work for Natural -/
+/-- Make + operator work for Natural ??? -/
 instance : Add Natural where
   add := add
+
+theorem addZeroLeftIsIdentity (n : Natural):
+add Natural.zero n = n
+ := by induction n with
+ | zero =>
+   calc add Natural.zero Natural.zero
+   = Natural.zero := by simp [add]
+ | next n' inductionHypothesis =>
+   calc add Natural.zero (Natural.next n')
+   = Natural.next (add Natural.zero n') := by simp [add]
+   _ = Natural.next n' := by rw [inductionHypothesis]
+
+theorem putNextAnywhereInAdd (n m : Natural):
+add n (Natural.next m) = add (Natural.next n) m
+  := by induction m with
+  | zero =>
+    calc add n (Natural.next Natural.zero)
+    = Natural.next (add n Natural.zero) := by simp[add]
+    _ = Natural.next n := by simp[add]
+    _ = add (Natural.next n) Natural.zero := by simp[add]
+  | next m' inductionHypothesis =>
+    calc add n (Natural.next (Natural.next m'))
+    = Natural.next (add n (Natural.next m')) := by simp[add]
+    _ = Natural.next (add (Natural.next n) m') := by rw[inductionHypothesis]
+    _ = add (Natural.next n) (Natural.next m') := by simp[add]
+
+theorem addIsCommutative (n m : Natural) : add n m = add m n := by
+ induction m with
+  | zero =>
+    calc add n Natural.zero
+    = n := by simp[add]
+    _ = add Natural.zero n := by rw [addZeroLeftIsIdentity]
+  | next m' inductionHypothesisM =>
+    calc add n (Natural.next m') =
+    Natural.next (add n m') := by simp[add]
+    _ = Natural.next (add m' n) := by rw[inductionHypothesisM]
+    _ = add m' (Natural.next n) := by simp[add]
+    _ = add (Natural.next m') n := by rw[putNextAnywhereInAdd]
+
+
 
 end HumanMath.Natural
