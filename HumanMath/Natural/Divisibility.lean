53c74ef8ca0c052d58e10df3c53d4afa43f5c110
chelokot <andrey.vlasenko.work@gmail.com>
2025-06-18 01:57:23 +0300

    add simple divisibility

---
+++ HumanMath/Natural/Divisibility.lean
@@ -0,0 +1,25 @@
+import HumanMath.Natural.Multiply
+
+namespace HumanMath.Natural
+
+def divides (a b : Natural) : Prop :=
+  ∃ k : Natural, a * k = b
+
+scoped infixl:50 " ∣ " => divides
+
+theorem zeroDividesZero:
+Natural.zero ∣ Natural.zero
+  := by
+  exists Natural.zero
+
+theorem zeroOnlyDividesZero (a : Natural) :
+  Natural.zero ∣ a → a = Natural.zero := by
+  intro premise
+  cases premise with
+  | intro k premiseForK =>
+    calc a
+      = Natural.zero * k := by rw[premiseForK]
+      _ = Natural.zero := by rw [leftMultiplyByZeroIsZero]
+
+
+end HumanMath.Natural
