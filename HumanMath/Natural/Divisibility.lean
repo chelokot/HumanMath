9496b5d975b131fbe32b7029728f696c314c4ef0
chelokot <andrey.vlasenko.work@gmail.com>
2025-06-18 02:14:07 +0300

    add basic prime definitions

--- HumanMath/Natural/Divisibility.lean
+++ HumanMath/Natural/Divisibility.lean
@@ -13,7 +13,8 @@ Natural.zero ∣ Natural.zero
   exists Natural.zero
 
 theorem zeroOnlyDividesZero (a : Natural) :
-  Natural.zero ∣ a → a = Natural.zero := by
+Natural.zero ∣ a → a = Natural.zero
+  := by
   intro premise
   cases premise with
   | intro k premiseForK =>
@@ -21,5 +22,19 @@ theorem zeroOnlyDividesZero (a : Natural) :
       = Natural.zero * k := by rw[premiseForK]
       _ = Natural.zero := by rw [leftMultiplyByZeroIsZero]
 
+def isUnit (a : Natural) : Prop :=
+  a ∣ Natural.one
+
+def isIrreducible (p : Natural) : Prop :=
+  ¬(p = Natural.zero) ∧ ¬(isUnit p) ∧ (
+    ∀ d : Natural,
+      d ∣ p → (isUnit d) ∨ (d = p)
+  )
+
+def isPrime (p : Natural) : Prop :=
+  ¬(p = Natural.zero) ∧ ¬(isUnit p) ∧ (
+    ∀ a b : Natural,
+      p ∣ a * b → (p ∣ a) ∨ (p ∣ b)
+  )
 
 end HumanMath.Natural
